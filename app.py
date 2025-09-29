461a5a85d4b500d1148ccfc93138afa0590e2732
TSekita <tatsuyukisekita749@gmail.com>
2025-09-29 20:35:20 +0900

    202509292035

--- app.py
+++ app.py
@@ -2,17 +2,11 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'   # セッション用の秘密鍵
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")  # 制御用に使用
 
 @app.route('/')
 def index():
     return render_template('index.html')  # templates/index.html を返す
-
-# SocketIOイベントの例
-@socketio.on('message')
-def handle_message(msg):
-    print('受信:', msg)
 
 if __name__ == '__main__':
     # Flask-SocketIOのサーバ起動
